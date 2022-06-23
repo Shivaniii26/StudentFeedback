1dcd5eadb223115db7ace228919dc85c35315606
Shivani Chaudhary <shivanichaudhary2605@gmail.com>
2022-06-23 21:04:14 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<div class="container">
+    <div class="uiet" class="jumbotron">
+        <div class="row" style="margin-bottom:40px;margin-left:50px;margin-top:15px">
+            <p>
+                This student feedback form is made to ease the process of collecting student feedback 
+                data which is more effective and time saving than traditional system to collect data 
+                and report will be available directly after analysis of feedback data. 
+            </p>
+
+        </div>
+    </div>
+</div>
---
+++ contact.php
@@ -0,0 +1,66 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <br>
+  <table>
+
+    <tr>
+      <td style="width: 18.5rem;">
+      </td>
+      <td style="width: 18.5rem;">
+      </td>
+      <td style="width: 12.5rem;"><img width=400px height=200px src="contactimg.jpg">
+      </td>
+    </tr>
+  </table>
+  <!-- center -->
+  <table style="margin-top:45px;">
+    <tr>
+      <td style="width: 18.5rem;">
+      </td>
+      <td style="width: 18.5rem;">
+      </td>
+
+      <td style="color:white;">
+        <center>
+          <strong>Office of the Director,<br />
+            University Institute of Engineering and Technology,<br />
+            CSJM University, Kanpur 208 024 (U.P.) INDIA</strong><br>
+          Phone Number : +91 (512) 258-0047 (office) <br>
+          Mobile Number: +91 9369601480 (office) <br>
+          E-mail : <em>administrator<strong>
+              <font style="color:red">@</font>
+            </strong>uietkanpur.org
+          </em>
+          <br /><br />
+
+        </center>
+      </td>
+      <td style="width: 15.5rem;">
+      </td>
+
+    </tr>
+  </table>
+  <br>
+  <table>
+    <tr>
+      <td style="width: 18.5rem;">
+      </td>
+      <td style="width: 18.5rem;">
+      </td>
+      <!-- <td style="width: 12.5rem;"><img width=400px height=175px src> -->
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
